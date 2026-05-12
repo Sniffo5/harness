abddd2d7e9bad04f342a72aca2b8c466027faeb1
Aron Loreskär <arolor07301@edu.halmstad.se>
2026-05-12 09:54:32 +0200

    style: formatting

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from "electron";
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+  });
 
-  win.loadFile('index.html')
-}
+  win.loadFile("index.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
